f8afddf3493d706c1d8589274d1b629ca540c85d
rajasegu <rajasegu@amazon.com>
2020-07-30 23:23:22 +0530

    Updating IAM roles and fixing oracle database schema creations

--- scripts/maximo_oracle_commands.sh
+++ scripts/maximo_oracle_commands.sh
@@ -17,26 +17,26 @@ unzip -o instantclient-sqlplus-linux.x64-19.6.0.0.0dbru.zip
 sudo yum install libaio
 
 # Update the runtime link path
-sudo sh -c "echo /home/ec2-user/instantclient_19_3 > \
+sudo sh -c "echo /home/ec2-user/instantclient_19_6 > \
    		/etc/ld.so.conf.d/oracle-instantclient.conf"
 sudo ldconfig
 
 # Set file and folder permissions / ownership
-chown -R ec2-user:ec2-user /home/ec2-user/instantclient_19_3
+chown -R ec2-user:ec2-user /home/ec2-user/instantclient_19_6
 chown ec2-user:ec2-user /home/ec2-user/maximo_oracle_commands.sh
 chown ec2-user:ec2-user /home/ec2-user/maximo_oracle_database_objects.sql
 chmod 755 /home/ec2-user/maximo_oracle_commands.sh
 
 
 # Build the sqlplus command
-Connection="/home/ec2-user/instantclient_19_3/sqlplus '$DBUser/$DBPassword@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$Endpoint)(PORT=$Port))(CONNECT_DATA=(SID=$DBName)))' @maximo_oracle_database_objects.sql"
+Connection="/home/ec2-user/instantclient_19_6/sqlplus '$DBUser/$DBPassword@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$Endpoint)(PORT=$Port))(CONNECT_DATA=(SID=$DBName)))' @maximo_oracle_database_objects.sql"
 
 # Write command to shell script file
 Executable="/home/ec2-user/create_objects.sh"
 echo "#!/bin/bash" > $Executable
-echo "LD_LIBRARY_PATH=/home/ec2-user/instantclient_19_3:$LD_LIBRARY_PATH" >> $Executable
+echo "LD_LIBRARY_PATH=/home/ec2-user/instantclient_19_6:$LD_LIBRARY_PATH" >> $Executable
 echo "export LD_LIBRARY_PATH" >> $Executable
-echo "PATH=$PATH:/home/ec2-user/instantclient_19_3" >> $Executable
+echo "PATH=$PATH:/home/ec2-user/instantclient_19_6" >> $Executable
 echo "export PATH" >> $Executable
 echo $Connection >> $Executable
 
